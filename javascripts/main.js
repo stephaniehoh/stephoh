3e827a8dada2bcf59dac3ffdb3342a4106eed6de
stephaniehoh <stephoh@gmail.com>
2014-01-08 11:06:54 -0500

    added hcaptions to built images for fade in overlay

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
   initSnap()
   $("p.email-show").hide()
   emailHover()
+  $('.hcaption').hcaptions({
+    height: 370,
+    width: 550,
+    overlay_bg: 'black',
+    overlay_opacity: 0.8
+  });
 })
 
 function initScroll(){
@@ -13,12 +19,12 @@ function initScroll(){
   })
 }
 
-function initSnap() {
+function initSnap(){
   $(document).scrollsnap({
-      snaps: 'section',
-      proximity: 125,
-      easing: 'easeOutQuart'
-    });
+    snaps: 'section',
+    proximity: 125,
+    easing: 'easeOutQuart'
+  });
 }
 
 function emailHover(){
